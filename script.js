0a7f8101a07b2984e9e4d178126ac3d198450352
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-05-12 21:28:01 +0100

    finish

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let URLNew = "https://restcountries.eu/rest/v2/all";
 const root = document.getElementById("root");
 const searchC = document.getElementById("searchInput");
+
 function setup(URL) {
     fetch(URLNew)
         .then((response) => {return response.json();})
-        .then((data) => {makePageForCountries(data);});
+        .then((data) => {select(data);});
 }
 function makePageForCountries(data){
+    removersFirstPage();
     data.forEach(element => {
             countries = document.createElement("div");
         const imageCountry = document.createElement("img");
         const nameCountry = document.createElement("h2");
         const population = document.createElement("p");
-        const region = document.createElement("p");
+        region = document.createElement("p");
         const capital = document.createElement("p");
 
         root.appendChild(countries);
@@ -22,6 +24,8 @@ function makePageForCountries(data){
         countries.appendChild(region);
         countries.appendChild(capital);
 
+        nameCountry.classList.add("nameC");
+        region.classList.add("region");
         countries.classList.add("countries");
         imageCountry.classList.add("image");
 
@@ -39,8 +43,10 @@ function makePageForCountries(data){
 function makeCountry(element){
     removersFirstPage();
     const bTn = document.createElement("button");
-    bTn.nativeName = `Back`;
-    root.appendChild(bTn);
+    bTn.classList.add("btn");
+    bTn.type = "Button";
+    bTn.innerText = "Back";
+    
     bTn.addEventListener(`click` , function(){
         removeSecundPage();
         bTn.style.display = `none`;
@@ -49,56 +55,70 @@ function makeCountry(element){
     const form2 = document.createElement(`form`);
     root.appendChild(form2);
     form2.classList.add(`form2`);
+    form2.appendChild(bTn);
     const countriesImg = document.createElement("div");
     const imageCountry = document.createElement("img");
 
     const countriesInfo = document.createElement("div");
-        nameCountry = document.createElement("h2");
+    const nameCountry2 = document.createElement("h2");
+
+    const countriesC = document.createElement("div");
     const population = document.createElement("p");
-    const region = document.createElement("p");
+    const region2 = document.createElement("p");
     const capital = document.createElement("p");
+
+    const countriesD = document.createElement("div");
     const capital2 = document.createElement("p");
     const capital3 = document.createElement("p");
     const capital4 = document.createElement("p");
 
-    const countriesB = document.createElement("div");
+    const countriesA = document.createElement("div");
     const nameB = document.createElement("p");
-
-    form2.appendChild(countriesImg);
-    form2.appendChild(countriesInfo);
-    form2.appendChild(countriesB);
-    countriesImg.appendChild(imageCountry);
-    countriesInfo.appendChild(nameCountry);
-    countriesInfo.appendChild(population);
-    countriesInfo.appendChild(region);
-    countriesInfo.appendChild(capital);
-    countriesInfo.appendChild(capital2);
-    countriesInfo.appendChild(capital3);
-    countriesInfo.appendChild(capital4);
-    countriesB.appendChild(nameB);
-    
 
     imageCountry.classList.add("big-image");
     countriesInfo.classList.add("countries-info");
-    countriesB.classList.add("countries-b");
+    region2.classList.add("region2");
+// appendchild the element //
+    bTn.appendChild(countriesImg);
+    form2.appendChild(countriesInfo);
+ 
+    countriesImg.appendChild(imageCountry);
 
+    countriesInfo.appendChild(nameCountry2);
+
+    countriesInfo.appendChild(countriesA);
+
+    countriesA.appendChild(countriesC);
+    countriesC.appendChild(population);
+    countriesC.appendChild(region2);
+    countriesC.appendChild(capital);
+
+    countriesA.appendChild(countriesD);
+    countriesD.appendChild(capital2);
+    countriesD.appendChild(capital3);
+    countriesD.appendChild(capital4);
+
+    countriesInfo.appendChild(nameB);
+    
     imageCountry.src = element.flag;
-    nameCountry.innerText = element.name;
-    capital.innerText =   `Native Name : ${element.nativeName};`;
+    nameCountry2.innerText = element.name;
+    capital.innerText = `Native Name : ${element.nativeName};`;
     population.innerText = `Population :${element.population}`;
-    region.innerText = `Region : ${element.region}`;
+    region2.innerText = `Region : ${element.region}`;
     capital2.innerText = `Top Level Domain : ${element.topLevelDomain}`;
     capital3.innerText = `Time Zones : ${element.timezones}`;
     capital4.innerText = `Currencies : ${element.currencies[0].name}`;
     nameB.innerText = `Borders with : ${element.borders}`;
 }
 function search(){
+
     const name = document.querySelectorAll(".countries");
+    console.log(name);
     let lowCase = searchC.value.toLowerCase();
     Array.from(name).forEach((element) => {
         let cCharacter = element.textContent.toLowerCase();
         if (cCharacter.indexOf(lowCase) != -1) {
-            element.style.display = "block";
+            countries.style.display = "block";
         } else {
             element.style.display = "none";
         }
@@ -106,8 +126,19 @@ function search(){
 }
 searchC.addEventListener("input", search);
 
+const dropL = document.getElementById("dropdowns");
 
-
+function select(data){
+    if (dropL.value === "-- All Region --"){
+        makePageForCountries(data);
+    }
+    else{
+        let selectX = dropL.value;
+        const z = data.filter(element => element.region === selectX );
+        makePageForCountries(z);
+    }
+}
+dropL.addEventListener("change", setup);
 
 function removersFirstPage(){
     let countryList = document.querySelectorAll(".countries");
